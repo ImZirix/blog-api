be4727327bfd59564a4aef9e3f8f992f7beb42ca
ImZirix <xxkillerxx.xg@gmail.com>
2025-05-07 16:28:55 +0300

    changed the cors URL for the frontend URL

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://blog-api-one-delta.vercel.app/" }));
 app.use(express.json());
 
 //Routes
