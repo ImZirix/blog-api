0ad4bfcd3bf134b10d89226ceaeaa85b83ee5624
ImZirix <xxkillerxx.xg@gmail.com>
2025-05-07 16:27:15 +0300

    added baseURL for the backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://blog-api-84d1.onrender.com/",
 });
 
 API.interceptors.request.use((req) => {
